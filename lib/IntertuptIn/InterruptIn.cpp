b8a63be659478d5cd0df031572e5616ab47279c3
Osawa <131154732+oswystk15662@users.noreply.github.com>
2023-10-14 12:30:26 +0900

    Update InterruptIn.cpp

--- lib/IntertuptIn/InterruptIn.cpp
+++ lib/IntertuptIn/InterruptIn.cpp
@@ -3,8 +3,8 @@
 InterruptIn::InterruptIn(PinName _pin):pin(_pin)
 {
     pinMode(pin, INPUT_PULLUP);
-    attachInterrupt(pin, rise, RISING);
-    attachInterrupt(pin, fall, FALLING);
+    attachInterrupt(pin, [this]{risefunc();}, RISING);
+    attachInterrupt(pin, [this]{fallfunc();}, FALLING);
 }
 
 InterruptIn::InterruptIn(PinName _pin, mPinMode _mpm):pin(_pin)
@@ -13,15 +13,9 @@ InterruptIn::InterruptIn(PinName _pin, mPinMode _mpm):pin(_pin)
 }
 
 void InterruptIn::rise(function<void()> _func){
-    detachInterrupt(pin);
-    _func();
-
-    attachInterrupt(pin, rise, RISING);
+    risefunc = _func;
 }
 
 void InterruptIn::fall(function<void()> _func){
-    detachInterrupt(pin);
-    _func();
-
-    attachInterrupt(pin, fall, FALLING);
+    fallfunc = _func;
 }
